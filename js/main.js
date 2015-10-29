d96289e658f71de48b684e6182ec028ebd67721d
Aly Fluckey <afluckey@2co.com>
2015-10-29 16:57:02 -0500

    adding active classes to items in sidebar on scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 $( document ).ready(function() {
+  var sections = $('.js-anchor')
+    , nav = $('#js-nav');
 
+  $(window).on('scroll', function () {
+    var cur_pos = $(this).scrollTop();
+
+    sections.each(function() {
+      var top = $(this).offset().top - 400,
+          bottom = top + $(window).height();
+
+      if (cur_pos >= top && cur_pos <= bottom) {
+        nav.find('a').removeClass('active');
+        sections.removeClass('active');
+
+        $(this).addClass('active');
+        nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+      }
+    });
+  });
 });

810b9605c28242f229709a32851d8beb4dc73d8e
Aly Fluckey <afluckey@2co.com>
2015-10-30 11:42:46 -0500

    adding scroll when they click the back to top link

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 $( document ).ready(function() {
+  $('.scroll-to-top').click(function () {
+    $("html, body").animate({
+        scrollTop: 0
+    }, 600);
+    return false;
+  });
+
   var sections = $('.js-anchor')
     , nav = $('#js-nav');
 
